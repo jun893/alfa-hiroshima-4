aaece824dfce5f527cda48fa46ea45c5320a4c3e
touka-130 <90014789+touka-130@users.noreply.github.com>
2024-01-12 12:14:52 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@ const app = Vue.createApp({
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'じんるいのせんべつ',
+          q1: 'ううう',
           // q2: 'かかか',
           // q3: 'ききき',
         }
@@ -128,5 +128,3 @@ app.component('answer-input', {
 })
 
 app.mount('#stage')
-
-
